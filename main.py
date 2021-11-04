354082f21d8309c8834fc11d4483cd8601669b19
ilhmwisnu <ilhamwisnu42@gmail.com>
2021-11-04 13:35:49 +0700

    Edit isi

--- main.py
+++ main.py
@@ -3,20 +3,32 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
-def kotak():
-    glScale(0.5,0.5,0.5)
+x_awan = 0.0
+
+
+def awan():
+    glScale(3,3,0)
+    glTranslated(210,170,0) 
+    glTranslate(x_awan,0,0)
     glBegin(GL_QUADS)
-    glVertex2f(-5,-5)
+    glVertex2f(-5,-5) 
     glVertex2f(5,-5)
     glVertex2f(5,5)
     glVertex2f(-5,5)
     glEnd()
+
+def timerAwan(value) :
+    global x_awan
+    if x_awan < -220 :
+        x_awan = 0
+    x_awan -= 1 
+    glutTimerFunc(100,timerAwan,0)
     
 def iterate():
     glViewport(0, 0, 600, 600)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    glOrtho(0.0, 30, 0.0, 30, 0.0, 1.0)
+    glOrtho(0.0, 600, 0.0, 600, 0.0, 1.0)
     glMatrixMode (GL_MODELVIEW)
     glLoadIdentity()
     
@@ -24,8 +36,7 @@ def showScreen():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
     iterate()
-    glColor3f(2, 0.3, 2)
-    kotak()    
+    awan()
     glutSwapBuffers()
 
 glutInit()
@@ -33,6 +44,7 @@ glutInitDisplayMode(GLUT_RGBA)
 glutInitWindowSize(600, 600)
 glutInitWindowPosition(1280//4, 0)
 wind = glutCreateWindow("The Adventure of Anoman")
+timerAwan(0)
 glutDisplayFunc(showScreen)
 glutIdleFunc(showScreen)
 glutMainLoop()
